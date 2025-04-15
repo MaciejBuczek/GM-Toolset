e36cfba44b675c328f217a148f1f1ef9b29a9b0a
Maciej Buczek <maciejbuczek98@gmail.com>
2025-04-15 17:04:25 +0200

    Implemented appinfo endpoint

--- src/Services/Character/Character.API/Program.cs
+++ src/Services/Character/Character.API/Program.cs
@@ -1,17 +1,28 @@
 using Common;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
 
-var test = builder.Configuration.GetConnectionString("Database");
-
 app.UseHttpsRedirection();
 
 app.MapGet("/common", () =>
 {
     return Sample.Test;
+});
+
+app.MapGet("/appinfo", () =>
+{
+    var appInfo = new
+    {
+        ServiceName = Assembly.GetExecutingAssembly().GetName().Name,
+        Environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
+        ConnectionString1 = builder.Configuration.GetConnectionString("Database")?.Length,
+    };
+
+    return appInfo;
 });
 
 app.Run();
