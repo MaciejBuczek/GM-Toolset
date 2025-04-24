eaafb305ac5c7f70007408c1aff4eb214050d440
Maciej Buczek <maciejbuczek98@gmail.com>
2025-04-24 09:41:39 +0200

    Empty create command handler class created

--- src/Common/Common/CQRS/ICommandHandler.cs
+++ src/Common/Common/CQRS/ICommandHandler.cs
@@ -2,7 +2,7 @@
 
 namespace Common.CQRS
 {
-    internal interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
         where TCommand : ICommand<TResponse>
         where TResponse : notnull
     {
---
+++ src/Services/Character/Character.API/Features/CreateCharacter/CreateCharacterCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using Character.API.Entities;
+using Common.CQRS;
+
+namespace Character.API.Features.CreateCharacter
+{
+    public record CreateCharacterResponse(Guid CharacterId);
+    public record CreateCharacterCommand(Guid UserId, Guid SchemaId, string Name, string Description, IEnumerable<Statistic> Statistics) : ICommand<CreateCharacterResponse>;
+
+    public class CreateCharacterCommandHandler : ICommandHandler<CreateCharacterCommand, CreateCharacterResponse>
+    {
+        public Task<CreateCharacterResponse> Handle(CreateCharacterCommand command, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+    }
+}
