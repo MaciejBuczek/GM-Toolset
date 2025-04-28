94fbba5690ff300f53e17f604009bd78e20a43a6
Maciej Buczek <maciejbuczek98@gmail.com>
2025-04-28 10:42:48 +0200

    Get characters by user id endpoint and handler implemented

---
+++ src/Services/Character/Character.API/Features/GetCharactersByUserId/GetCharactersByUserIdEndpoint.cs
@@ -0,0 +1,28 @@
+﻿namespace Character.API.Features.GetCharactersByUserId
+{
+    public record GetCharactersByUserIdResponse(IEnumerable<Entities.Character> Characters);
+
+    public class GetCharactersByUserIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/characters/user/{id}",
+                async (Guid id,
+                IRequestHandler<GetCharactersByUserIdQuery,
+                GetCharactersByUserIdResult> handler,
+                CancellationToken cancellationToken) =>
+                {
+                    var command = new GetCharactersByUserIdQuery(id);
+                    var result = await handler.Handle(command, cancellationToken);
+                    var response = result.Adapt<GetCharactersByUserIdResponse>();
+
+                    return Results.Ok(response);
+                })
+                .WithName("GetCharacterByUserId")
+                .WithTags("Character")
+                .Produces<CreateCharacterResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithOpenApi();
+        }
+    }
+}
