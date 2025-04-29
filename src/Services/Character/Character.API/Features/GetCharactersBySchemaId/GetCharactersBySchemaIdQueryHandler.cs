d3255b41d7048aa19b233c3671f583edf40344b3
Maciej Buczek <maciejbuczek98@gmail.com>
2025-04-29 08:05:36 +0200

    Changed parameter name from request to query for character get handlers

--- src/Services/Character/Character.API/Features/GetCharactersBySchemaId/GetCharactersBySchemaIdQueryHandler.cs
+++ src/Services/Character/Character.API/Features/GetCharactersBySchemaId/GetCharactersBySchemaIdQueryHandler.cs
@@ -4,9 +4,9 @@
     public record GetCharactersBySchemaIdQuery(Guid Id) : IQuery<GetCharactersBySchemaIdResult>;
     public class GetCharactersBySchemaIdQueryHandler(IDocumentSession session) : IQueryHandler<GetCharactersBySchemaIdQuery, GetCharactersBySchemaIdResult>
     {
-        public async Task<GetCharactersBySchemaIdResult> Handle(GetCharactersBySchemaIdQuery request, CancellationToken cancellationToken = default)
+        public async Task<GetCharactersBySchemaIdResult> Handle(GetCharactersBySchemaIdQuery query, CancellationToken cancellationToken = default)
         {
-            var characters = await session.Query<Entities.Character>().Where(c => c.SchemaId.Equals(request.Id)).ToListAsync(cancellationToken);
+            var characters = await session.Query<Entities.Character>().Where(c => c.SchemaId.Equals(query.Id)).ToListAsync(cancellationToken);
             return characters is null || characters.IsEmpty()
                 ? throw new ApplicationException("Characters not found")
                 : new GetCharactersBySchemaIdResult(characters);
--- src/Services/Character/Character.API/Features/GetCharactersByUserId/GetCharactersByUserIdHandler.cs
+++ src/Services/Character/Character.API/Features/GetCharactersByUserId/GetCharactersByUserIdHandler.cs
@@ -4,9 +4,9 @@
     public record GetCharactersByUserIdQuery(Guid Id) : IQuery<GetCharactersByUserIdResult>;
     public class GetCharactersByUserIdQueryHandler(IDocumentSession session) : IQueryHandler<GetCharactersByUserIdQuery, GetCharactersByUserIdResult>
     {
-        public async Task<GetCharactersByUserIdResult> Handle(GetCharactersByUserIdQuery request, CancellationToken cancellationToken = default)
+        public async Task<GetCharactersByUserIdResult> Handle(GetCharactersByUserIdQuery query, CancellationToken cancellationToken = default)
         {
-            var characters = await session.Query<Entities.Character>().Where(c => c.UserId.Equals(request.Id)).ToListAsync(cancellationToken);
+            var characters = await session.Query<Entities.Character>().Where(c => c.UserId.Equals(query.Id)).ToListAsync(cancellationToken);
             return characters is null || characters.IsEmpty()
                 ? throw new ApplicationException("Characters not found")
                 : new GetCharactersByUserIdResult(characters);
