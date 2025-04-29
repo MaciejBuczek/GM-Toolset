ca1a3f49f37efaa77262599ce0e708781d6dc4a6
Maciej Buczek <maciejbuczek98@gmail.com>
2025-04-29 08:04:36 +0200

    Delete character by id endpoint and handler implemented

---
+++ src/Services/Character/Character.API/Features/DeleteCharacterById/DeleteCharacterByIdEndpoint.cs
@@ -0,0 +1,28 @@
+﻿namespace Character.API.Features.DeleteCharacterById
+{
+    public class DeleteCharacterByIdEndpoint : ICarterModule
+    {
+        public record DeleteCharacterByIdResponse(bool Success);
+
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/characters/{id}",
+                async (Guid id,
+                IRequestHandler<DeleteCharacterByIdCommand,
+                DeleteCharacterByIdResult> handler,
+                CancellationToken cancellationToken) =>
+                {
+                    var command = new DeleteCharacterByIdCommand(id);
+                    var result = await handler.Handle(command, cancellationToken);
+                    var response = result.Adapt<DeleteCharacterByIdResponse>();
+
+                    return Results.Ok(response);
+                })
+                .WithName("DeleteCharacterById")
+                .WithTags("Character")
+                .Produces<DeleteCharacterByIdResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .WithOpenApi();
+        }
+    }
+}
