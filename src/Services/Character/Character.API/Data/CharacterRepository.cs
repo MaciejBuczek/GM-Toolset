a9def4db8ea4e5f34d251d3e725b5f05a7cc1b6d
Maciej Buczek <maciejbuczek98@gmail.com>
2025-05-12 15:09:34 +0200

    Character repository interface and class implemented

---
+++ src/Services/Character/Character.API/Data/CharacterRepository.cs
@@ -0,0 +1,54 @@
+﻿namespace Character.API.Data
+{
+    internal class CharacterRepository(IDocumentSession session) : ICharacterRepository
+    {
+        public async Task<Guid> CreateCharacter(Entities.Character character, CancellationToken cancellationToken)
+        {
+            session.Store(character);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return character.Id;
+        }
+
+        public async Task<bool> DeleteCharacter(Entities.Character character, CancellationToken cancellationToken)
+        {
+            session.Delete(character);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return true;
+        }
+
+        public async Task<Entities.Character?> GetCharacterById(Guid id, CancellationToken cancellationToken)
+        {
+            var character = await session.LoadAsync<Entities.Character>(id, cancellationToken);
+
+            return character;
+        }
+
+        public async Task<IEnumerable<Entities.Character>> GetCharacterBySchemaId(Guid schemaId, CancellationToken cancellationToken)
+        {
+            var characters = await session.Query<Entities.Character>()
+                .Where(c => c.SchemaId.Equals(schemaId))
+                .ToListAsync();
+
+            return characters;
+        }
+
+        public async Task<IEnumerable<Entities.Character>> GetCharacterByUserId(Guid userId, CancellationToken cancellationToken)
+        {
+            var characters = await session.Query<Entities.Character>()
+                .Where(c => c.UserId.Equals(userId))
+                .ToListAsync();
+
+            return characters;
+        }
+
+        public async Task<bool> UpdateCharacter(Entities.Character character, CancellationToken cancellationToken)
+        {
+            session.Update(character);
+            await session.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
---
+++ src/Services/Character/Character.API/Data/ICharacterRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Character.API.Data
+{
+    internal interface ICharacterRepository
+    {
+        Task<Entities.Character?> GetCharacterById(Guid id, CancellationToken cancellationToken);
+        Task<IEnumerable<Entities.Character>> GetCharacterByUserId(Guid userId, CancellationToken cancellationToken);
+        Task<IEnumerable<Entities.Character>> GetCharacterBySchemaId(Guid schemaId, CancellationToken cancellationToken);
+        Task<bool> UpdateCharacter(Entities.Character character, CancellationToken cancellationToken);
+        Task<bool> DeleteCharacter(Entities.Character character, CancellationToken cancellationToken);
+        Task<Guid> CreateCharacter(Entities.Character character, CancellationToken cancellationToken);
+    }
+}
